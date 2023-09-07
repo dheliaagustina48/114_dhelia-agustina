5ba638b9d97e18c72847995977cc91aaaba42d80
dheliaagustina48 <144302561+dheliaagustina48@users.noreply.github.com>
2023-09-07 10:37:32 +0700

    Add files via upload

---
+++ penugasan1.py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Tabung
+
+print("Program Menghitung Volume Tabung")
+
+Pi = 3.14
+r = int(input("Masukkan Jari Jari Tabung : "))
+t = int(input("Masaukkan Tinggi Tabung : "))
+
+V = Pi * r ** 2 * t
+print("Rumus Menghitung Volume Tabung V = Pi x r**2 x t")
+print("Hasil dari Hitung Volume Tabung : ",V)
---
+++ penugasan2.py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Balok
+
+print("Prrogram Menghitung Balok")
+
+p = int(input("Masukkan Panjanhg Balok : "))
+l = int(input("Masukkan Lebar Balok : "))
+t = int(input("Maskkan Tinggi Balok : "))
+
+V = p * l * t
+print("Rumus Menghitung Volume Balok V = p x l t")
+print("Hasil dari Hitng Balok : ",V)
